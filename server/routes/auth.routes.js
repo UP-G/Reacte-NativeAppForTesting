ee3200ea663a94b94fc823de40189ec0b82b96ea
Andrey C <ncix2012@gmail.com>
2022-04-27 02:13:57 +0300

    add login

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,11 +1,13 @@
 const Router = require('express');
 const User = require('../models/User');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const {check, validationResult} = require('express-validator');
 
 const router = new Router();
-//Сделать JWT токены
-router.post('/regUser',
+
+router.post('/reg',
     [
         check('email', 'Uncur Email').isEmail(),
         check('password', 'min 4 symbol max 16 symbol ').isLength({min: 4, max: 16}),
@@ -30,6 +32,36 @@ router.post('/regUser',
             const user = new User({name, login, email, password: hasPassword});
             await user.save();
             return res.json({message: 'User ready'});
+
+        } catch (e) {
+            console.log(e);
+            res.send({message: 'Error'});
+        }
+    },
+);
+
+router.post('/login',
+    async (req, res) => {
+        try {
+            const {login, password} = req.body;
+            const user = await User.findOne({login});
+            if (!user){
+                return res.status(404).json({message: `Пользователя с таким ${login} не существует`});
+            }
+            const passValid = bcrypt.compareSync(password, user.password);
+            if (!passValid){
+                return res.status(400).json({message: "Неверный пароль"});
+            }
+            const token = jwt.sign({id: user.id}, config.get('secretKey'), {expiresIn: "24h"});
+            return res.json({
+              token,
+              user: {
+                  id: user.id,
+                  login: user.login,
+                  email: user.email,
+                  name: user.name
+              }
+            })
 
         } catch (e) {
             console.log(e);
