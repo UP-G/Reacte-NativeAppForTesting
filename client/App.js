b3c3e27290994f880f619a8fba45b999f496ca0d
Andrey C <ncix2012@gmail.com>
2022-04-27 03:03:26 +0300

    add simple component

--- client/App.js
+++ client/App.js
@@ -1,66 +1,14 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-  TextInput,
-  Button,
-} from 'react-native';
-import {Formik} from 'formik';
+import React, {Component} from "react";
+import Auth from "./src/components/authUser/auth";
 
-const App = () => {
-  return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={styles.container}>
-        <View style={styles.loginContainer}>
-          <Text>Login Screen</Text>
-          <Formik
-            initialValues={{email: '', password: ''}}
-            onSubmit={values => console.log(values)}>
-            {({handleChange, handleBlur, handleSubmit, values}) => (
-              <>
-                <TextInput
-                  name="email"
-                  placeholder="Email Address"
-                  style={styles.textInput}
-                  onChangeText={handleChange('email')}
-                  onBlur={handleBlur('email')}
-                  value={values.email}
-                  keyboardType="email-address"
-                />
-                <TextInput
-                  name="password"
-                  placeholder="Password"
-                  style={styles.textInput}
-                  onChangeText={handleChange('password')}
-                  onBlur={handleBlur('password')}
-                  value={values.password}
-                  secureTextEntry
-                />
-                <Button onPress={handleSubmit} title="Submit" />
-              </>
-            )}
-          </Formik>
-        </View>
-      </SafeAreaView>
-    </>
-  );
-};
+export default class App extends Component {
+  // state = {
+  //
+  // }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  loginContainer: {
-    width: '80%',
-    alignItems: 'center',
-  },
-});
-
-export default App;
+  render() {
+    return (
+        <Auth />
+    )
+  }
+}
---
+++ client/src/components/authUser/auth.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+    SafeAreaView,
+    StyleSheet,
+    ScrollView,
+    View,
+    Text,
+    StatusBar,
+    TextInput,
+    Button,
+} from 'react-native';
+import {Formik} from 'formik';
+
+const Auth = () => {
+    return (
+        <>
+            <StatusBar barStyle="dark-content" />
+            <SafeAreaView style={styles.container}>
+                <View style={styles.loginContainer}>
+                    <Text>Login Screen</Text>
+                    <Formik
+                        initialValues={{email: '', password: ''}}
+                        onSubmit={values => console.log(values)}>
+                        {({handleChange, handleBlur, handleSubmit, values}) => (
+                            <>
+                                <TextInput
+                                    name="email"
+                                    placeholder="Email Address"
+                                    style={styles.textInput}
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                    value={values.email}
+                                    keyboardType="email-address"
+                                />
+                                <TextInput
+                                    name="password"
+                                    placeholder="Password"
+                                    style={styles.textInput}
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    value={values.password}
+                                    secureTextEntry
+                                />
+                                <Button onPress={handleSubmit} title="Submit" />
+                            </>
+                        )}
+                    </Formik>
+                </View>
+            </SafeAreaView>
+        </>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    loginContainer: {
+        width: '80%',
+        alignItems: 'center',
+    },
+});
+
+export default Auth;
